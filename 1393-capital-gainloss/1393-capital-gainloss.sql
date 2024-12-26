61d71528d4f3b0d2a93da790335cdbf31ccc2ca8
yesolee <58902084+yesolee@users.noreply.github.com>
2024-12-26 12:06:12 +0900

    Time: 1222 ms (7.53%), Space: 0B (100%) - LeetHub

--- 1393-capital-gainloss/1393-capital-gainloss.sql
+++ 1393-capital-gainloss/1393-capital-gainloss.sql
@@ -1,12 +1,23 @@
-with grouped as (select stock_name, operation, sum(price) as price
+select stock_name,
+    sum(case 
+            when operation = "BUY" then -price
+            else price
+        end
+    ) as capital_gain_loss
 from Stocks
-group by stock_name, operation
-order by stock_name, operation)
+group by stock_name
 
-select a.stock_name, 
-    (a.price - b.price) as capital_gain_loss
-from grouped a
-join grouped b
-on a.stock_name = b.stock_name
-and (a.operation = "Sell" and b.operation = "Buy")
+
+
+-- with grouped as (select stock_name, operation, sum(price) as price
+-- from Stocks
+-- group by stock_name, operation
+-- order by stock_name, operation)
+
+-- select a.stock_name, 
+--     (a.price - b.price) as capital_gain_loss
+-- from grouped a
+-- join grouped b
+-- on a.stock_name = b.stock_name
+-- and (a.operation = "Sell" and b.operation = "Buy")
 
